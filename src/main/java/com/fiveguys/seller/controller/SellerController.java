4ccbc06b1c675eb0984a30b532442f0939604da2
qweqeqqweqe <semin06133@naver.com>
2024-06-13 11:39:03 +0900

    sellerCommunity

--- src/main/java/com/fiveguys/seller/controller/SellerController.java
+++ src/main/java/com/fiveguys/seller/controller/SellerController.java
@@ -11,4 +11,10 @@ public class SellerController {
     public String mainPage() {
         return "seller/mainPage";
     }
+
+    @RequestMapping("sellerCommunity")
+    public String sellerCommunity(){
+        return "seller/sellerCommunity";
+    }
+
 }

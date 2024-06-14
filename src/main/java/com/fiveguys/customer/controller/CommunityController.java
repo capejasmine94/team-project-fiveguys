889a7925da907c31e00b9248e01cb27b0d5e2b61
JH12346 <kimks9292@naver.com>
2024-06-14 10:00:41 +0900

    .

--- src/main/java/com/fiveguys/customer/controller/CommunityController.java
+++ src/main/java/com/fiveguys/customer/controller/CommunityController.java
@@ -1,8 +1,38 @@
 package com.fiveguys.customer.controller;
 
+import com.fiveguys.customer.service.CommunityService;
+import com.fiveguys.dto.CommunityDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Map;
 
 @Controller
+@RequestMapping("customer")
 public class CommunityController {
+
+    private CommunityService communityService;
+
+    @RequestMapping("communityPage")
+    public String communityPage(Model model){
+        List<Map<String, Object>> selectCommunityList = communityService.selectCommunityList();
+
+        model.addAttribute("selectCommunityList", selectCommunityList);
+
+        return "redirect:./communityPage";
+    }
+
+    @RequestMapping("communityWritePage")
+    public String communityWritePage(){
+
+        return "customer/communityWritePage";
+    }
+
+
+
+
+
 
 }

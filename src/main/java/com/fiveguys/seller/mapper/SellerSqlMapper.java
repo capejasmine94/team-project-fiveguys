5edc5c273dc816569c52a7ce64ae7fdd81f28906
capejasmine94 <123536114+capejasmine94@users.noreply.github.com>
2024-06-14 15:36:24 +0900

    매장선택수정중

--- src/main/java/com/fiveguys/seller/mapper/SellerSqlMapper.java
+++ src/main/java/com/fiveguys/seller/mapper/SellerSqlMapper.java
@@ -1,12 +1,11 @@
 package com.fiveguys.seller.mapper;
 
-import com.fiveguys.dto.SellerDto;
 import org.apache.ibatis.annotations.Mapper;
 
-import java.util.List;
+
 
 @Mapper
 public interface SellerSqlMapper {
 
-    public List<SellerDto> selectAllSellers();
+
 }

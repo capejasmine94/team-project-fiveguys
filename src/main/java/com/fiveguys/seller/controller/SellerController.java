b5d41ef6a82e3057b15766f0a4c240cc79d18ef5
Uhyeok99 <galaxykwh@naver.com>
2024-06-14 11:47:20 +0900

    재료메뉴페이지 완성
    후하

--- src/main/java/com/fiveguys/seller/controller/SellerController.java
+++ src/main/java/com/fiveguys/seller/controller/SellerController.java
@@ -19,9 +19,35 @@ public class SellerController {
         return "seller/orderPage";
     }
 
-    @RequestMapping("materialMenuPage")
-    public String materialMenuPage() {
-        return "seller/materialMenuPage";
+    @RequestMapping("materialMenuPage1")
+    public String materialMenuPage1() {
+        return "/seller/materialMenuPage1";
+    }
+
+    @RequestMapping("materialMenuPage2")
+    public String materialMenuPage2() {
+        return "/seller/materialMenuPage2";
+    }
+
+    @RequestMapping("materialMenuPage3")
+    public String materialMenuPage3() {
+        return "/seller/materialMenuPage3";
+    }
+
+    @RequestMapping("materialMenuPage5")
+    public String materialMenuPage5() {
+        return "/seller/materialMenuPage5";
+    }
+
+    @RequestMapping("materialMenuPage6")
+    public String materialMenuPage6() {
+        return "/seller/materialMenuPage6";
+    }
+
+
+    @RequestMapping("materialMenuPage4")
+    public String materialMenuPage4() {
+        return "/seller/materialMenuPage4";
     }
 
 

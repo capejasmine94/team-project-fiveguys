1fa82496a03fa6289481ac0c062c667390163f38
capejasmine94 <123536114+capejasmine94@users.noreply.github.com>
2024-06-18 08:55:20 +0900

    구매자메뉴

--- src/main/java/com/fiveguys/seller/mapper/SellerCustomerSqlMapper.java
+++ src/main/java/com/fiveguys/seller/mapper/SellerCustomerSqlMapper.java
@@ -1,11 +1,13 @@
 package com.fiveguys.seller.mapper;
 
 import com.fiveguys.dto.CategoryDto;
+import com.fiveguys.dto.ProductAndCategoryDto;
 import com.fiveguys.dto.ProductDto;
 import com.fiveguys.dto.SellerDto;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface SellerCustomerSqlMapper {
@@ -18,5 +20,8 @@ public interface SellerCustomerSqlMapper {
 
     public CategoryDto selectCategoryByNumber(int categoryNumber);
     public List<ProductDto> selectProductList(int categoryNumber);
+
+    public ProductDto selectProductDetail(ProductDto productDto);
+    public List<ProductAndCategoryDto> selectProductsByCategoryNumbers(List<Integer> categoryNumbers);
 
 }

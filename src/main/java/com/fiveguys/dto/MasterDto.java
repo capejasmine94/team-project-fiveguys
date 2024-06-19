b1e34d09ab879293ead42fc5e255bc87b911cd97
Uhyeok99 <galaxykwh@naver.com>
2024-06-19 16:53:38 +0900

    이벤트

--- src/main/java/com/fiveguys/dto/MasterDto.java
+++ src/main/java/com/fiveguys/dto/MasterDto.java
@@ -9,5 +9,6 @@ public class MasterDto {
     private int masterNumber;
     private String masterId;
     private String masterPassword;
+    private String masterNickName;
     private Date materCreatedAt;
 }
